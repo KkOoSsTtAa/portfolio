015ed95a7a94e4969dbe7d042935c8887d7d6845
KkOoSsTtAa <nemanja.kostadinovic9@gmail.com>
2023-05-11 18:20:50 +0200

    circles

--- src/components/BackgroundCircles.tsx
+++ src/components/BackgroundCircles.tsx
@@ -18,11 +18,9 @@ const BackgroundCircles = ({
       <div
         className={`fixed left-1/2 right-[-80vw] top-[-45vmax] h-[80vmax] w-[80vmax] rounded-full bg-myyellow transition-all duration-700 ease-in-out ${
           inView1 ? "translate-x-[-50%]" : ""
-        } ${
-          inView2 || inView3 || inView4
-            ? "-translate-x-[30vh] -translate-y-[60vh]"
-            : ""
-        } ${inView5 ? "-translate-x-[30vh] -translate-y-[60vh]" : ""}`}
+        } ${inView2 ? "-translate-x-[30vh] -translate-y-[60vh]" : ""} ${
+          inView3 || inView4 ? "-translate-x-[30vh] -translate-y-[90vh]" : ""
+        } ${inView5 ? "-translate-x-[30vh] -translate-y-[50vh]" : ""}`}
       />
       <div
         className={`fixed -left-[30%] top-[80%] h-[60vmax] w-[60vmax] rounded-full bg-myblack transition-all duration-700 ease-in-out ${
@@ -31,11 +29,11 @@ const BackgroundCircles = ({
             : ""
         } ${
           inView2
-            ? "h-[90vmax] w-[90vmax] -translate-y-[110vmax] translate-x-[0vmax] transform sm:-translate-y-[100vmax] sm:translate-x-[26vmax] md:-translate-y-[105vmax] xl:-translate-y-[105vmax]"
+            ? "h-[150vmax] w-[150vmax] -translate-y-[130%] translate-x-[0%] transform lg:-translate-y-[125%] xl:-translate-y-[120%] 2xl:-translate-y-[115%]"
             : ""
         } ${
           inView3 || inView4
-            ? "translate-x-[20vmax] translate-y-[-105vmax] transform md:translate-x-[40vmax] md:translate-y-[-100vmax] lg:translate-x-[50vmax] lg:translate-y-[-85vmax]"
+            ? "translate-x-[40%] translate-y-[-180%] md:translate-x-[80%] lg:translate-y-[-160%] xl:translate-y-[-150%]"
             : ""
         } ${inView5 ? "translate-x-[20vh] translate-y-[5vh]" : ""}`}
       />
