3cd8ebdae2865672c86bb185f24de2c306684160
KkOoSsTtAa <nemanja.kostadinovic9@gmail.com>
2023-05-14 11:59:35 +0200

    changed circles to motion.div, added useWindowSize

--- src/components/BackgroundCircles.tsx
+++ src/components/BackgroundCircles.tsx
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import React from "react";
+import useWindowSize from "~/hooks/useWindowSize";
 
 type BackgroundCirclesProps = {
   inView: {
@@ -12,37 +14,110 @@ type BackgroundCirclesProps = {
 };
 
 const BackgroundCircles = ({
-  inView: { inView0, inView1, inView2, inView3, inView4, inView5 },
+  inView: { inView1, inView2, inView3, inView4, inView5 },
 }: BackgroundCirclesProps) => {
-  const black0 = "left-[-25%] top-[80%] h-[50vmax] w-[50vmax]";
-  const black1 =
-    "left-[50%] top-[95%] h-[25vmax] w-[25vmax] translate-x-[-50%]";
-  const black2 =
-    "left-[50%] top-[-15%] h-[45vmax] w-[45vmax] translate-x-[-50%] lg:top-[-40%]";
-  const black34 =
-    "left-[50%] top-[-60%] h-[75vmax] w-[75vmax] translate-x-[-50%] lg:top-[-50%] lg:h-[35vmax] lg:w-[35vmax]";
-  const black5 =
-    "left-[50%] top-[80%] h-[105vmax] w-[105vmax] translate-x-[-50%]";
+  const { width } = useWindowSize();
+
+  const style0 = {
+    left: "-25%",
+    top: "80%",
+    height: "50vmax",
+    width: "50vmax",
+  };
+
+  const style1 = {
+    left: "50%",
+    top: "95%",
+    height: "25vmax",
+    width: "25vmax",
+    translateX: "-50%",
+  };
+
+  const style2 =
+    width && width > 1024
+      ? {
+          left: "50%",
+          top: "-40%",
+          height: "45vmax",
+          width: "45vmax",
+          translateX: "-50%",
+        }
+      : {
+          left: "50%",
+          top: "-15%",
+          height: "45vmax",
+          width: "45vmax",
+          translateX: "-50%",
+        };
+
+  const style34 =
+    width && width > 1024
+      ? {
+          left: "50%",
+          top: "-50%",
+          height: "35vmax",
+          width: "35vmax",
+          translateX: "-50%",
+        }
+      : {
+          left: "50%",
+          top: "-60%",
+          height: "75vmax",
+          width: "75vmax",
+          translateX: "-50%",
+        };
+
+  const style5 = {
+    left: "50%",
+    top: "80%",
+    height: "105vmax",
+    width: "105vmax",
+    translateX: "-50%",
+  };
 
   return (
     <>
-      <div
-        className={`fixed left-[50%] top-[-35vmax] h-[75vmax] w-[75vmax] rounded-full bg-myyellow transition-all duration-700 ease-in-out ${
+      <motion.div
+        className="fixed rounded-full bg-myyellow transition-all duration-700 ease-in-out"
+        initial={{
+          left: "50%",
+          top: "-35vmax",
+          height: "75vmax",
+          width: "75vmax",
+        }}
+        animate={
           inView1
-            ? "top-[-100vmax] h-[130vmax] w-[130vmax] translate-x-[-50%]"
-            : ""
-        } ${
-          inView2 || inView3 || inView4 || inView5
-            ? "top-[-100vmax] h-[130vmax] w-[130vmax] translate-x-[-50%] translate-y-[-50%]"
-            : ""
-        } `}
+            ? {
+                top: "-100vmax",
+                height: "130vmax",
+                width: "130vmax",
+                translateX: "-50%",
+              }
+            : inView2 || inView3 || inView4 || inView5
+            ? {
+                top: "-100vmax",
+                height: "130vmax",
+                width: "130vmax",
+                translateX: "-50%",
+                translateY: "-50%",
+              }
+            : {}
+        }
       />
-      <div
-        className={`fixed rounded-full bg-myblack transition-all duration-700 ease-in-out ${
-          inView0 ? black0 : ""
-        } ${inView1 ? black1 : ""} ${inView2 ? black2 : ""} ${
-          inView3 || inView4 ? black34 : ""
-        } ${inView5 ? black5 : ""}`}
+      <motion.div
+        className="fixed rounded-full bg-myblack transition-all duration-700 ease-in-out "
+        initial={style0}
+        animate={
+          inView1
+            ? style1
+            : inView2
+            ? style2
+            : inView3 || inView4
+            ? style34
+            : inView5
+            ? style5
+            : {}
+        }
       />
     </>
   );
