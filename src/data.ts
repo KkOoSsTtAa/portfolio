f803fdeff2a3f6e1d23540e1a9c712a6dd920989
KkOoSsTtAa <nemanja.kostadinovic9@gmail.com>
2024-05-31 22:34:42 +0200

    small text changes

--- src/data.ts
+++ src/data.ts
@@ -16,7 +16,7 @@ export const TEXT_ABOUT_1 =
   "My programming journey began in High School, where I learned Pascal and enjoyed it a lot. I continued to study C, C++, and C# in college, but I did not complete my degree due to life challenges. However, my programming passion remained strong.";
 
 export const TEXT_ABOUT_2 =
-  "I began learning web development about one and a half years ago and I am passionate about expanding my skills and knowledge. However, I sometimes feel that there is not enough time in life to learn everything I want to learn.";
+  "I began learning web development in early 2022 and I am passionate about expanding my skills and knowledge. However, I sometimes feel that there is not enough time in life to learn everything I want to learn.";
 
 export const ABOUT_MY_SKILLS = "My skills";
 
@@ -34,7 +34,7 @@ export const TEXT_FIRST_PROJ_1 =
   "Project ORDER is my first web application that allows users to order food from a restaurant. It is made in mid 2022. It is a full-stack application that I developed on my own. It is not finished yet.";
 
 export const TEXT_FIRST_PROJ_2 =
-  "I was amazed by Next.JS, and I wanted to try it out. I am impressed by its recent updates and eager to see how it will influence not only React, but also the entire web development community.";
+  "I was amazed by Next.JS, and I wanted to try it out. I am impressed by its recent updates and eager to see how it will change usage of React, but also the entire web development community.";
 
 // export const URL_FIRST_PROJ = "https://order.kkoossttaa.com/";
 
