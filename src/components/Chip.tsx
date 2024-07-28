c6e9a1673da68046fdd494e84216f8843d8d2d9c
KkOoSsTtAa <nemanja.kostadinovic9@gmail.com>
2024-07-28 14:04:33 +0200

    fixing chips

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -11,7 +11,7 @@ export const Chip = ({ children }: ChipProps) => {
   return (
     <span
       className={cn(
-        "line-clamp-1 w-fit min-w-fit flex-1 rounded-[25px] bg-myBlack px-3 py-1 text-center text-xs tracking-wide text-white md:text-sm",
+        "shrink-0 rounded-[25px] bg-myBlack px-3 py-1 text-center text-xs tracking-wide text-white md:text-sm",
         fahkwang.className,
       )}
     >
